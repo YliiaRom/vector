ca5f3099d02ea64db53e0d0f2e736b115088bbf3
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2026-01-20 08:40:00 +0200

    add btn

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,11 @@
+import css from "./Calculator.module.css";
+
+function Calculator() {
+  return (
+    <>
+      <h1 className={css.mainBtn}>Vector</h1>
+    </>
+  );
+}
+
+export default Calculator;
