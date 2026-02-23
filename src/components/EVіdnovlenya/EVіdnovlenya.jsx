53f01c7ab35daf40250676f4c4d476a97d73c0c9
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2026-02-23 11:56:45 +0200

    add link

--- src/components/EVіdnovlenya/EVіdnovlenya.jsx
+++ src/components/EVіdnovlenya/EVіdnovlenya.jsx
@@ -1,7 +1,8 @@
+import css from "./EVіdnovlenya.module.css";
 function EVіdnovlenya() {
   return (
     <>
-      <div>
+      <div className={css.box}>
         <h2>еВідновлення</h2>
         <h3>ПОСТАНОВА від 21 квітня 2023 р. № 381</h3>
         <p>
@@ -11,21 +12,31 @@ function EVіdnovlenya() {
           Російської Федерації, з використанням електронної публічної послуги
           "єВідновлення"
         </p>
+        <button>
+          <a
+            href="https://erecovery.diia.gov.ua/destroyed.html"
+            target="_blank"
+          >
+            Офіційний сайт програми єВідновлення (Дія)
+          </a>
+        </button>
 
-        <a href="https://erecovery.diia.gov.ua/destroyed.html" target="_blank">
-          Офіційний сайт програми єВідновлення (Дія)
-        </a>
         <h3>КАБІНЕТ МІНІСТРІВ УКРАЇНИ ПОСТАНОВА від 30 травня 2023 р. № 600</h3>
-        <a
-          href="https://zakon.rada.gov.ua/laws/show/600-2023-%D0%BF#Text"
-          target="_blank"
-        >
-          посилання на постанову № 600 (єВідновлення)
-        </a>
+        <button>
+          <a
+            href="https://zakon.rada.gov.ua/laws/show/600-2023-%D0%BF#Text"
+            target="_blank"
+          >
+            посилання на постанову № 600 (єВідновлення)
+          </a>
+        </button>
+
         <h3>еОселя</h3>
-        <a href="https://ukrfinzhytlo.in.ua/e-oselia/" target="_blank">
-          посилання на программу іпотечного кредитування житла (єОселя)
-        </a>
+        <button>
+          <a href="https://ukrfinzhytlo.in.ua/e-oselia/" target="_blank">
+            посилання на программу іпотечного кредитування житла (єОселя)
+          </a>
+        </button>
       </div>
     </>
   );
